ae044b3c8c284e17cc4aa48cb79942c7d7bee968
YeThura-424 <rathanpoudle.26@gmail.com>
2023-09-20 12:12:00 +0630

    welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,159 +1,155 @@
 <x-backend>
 
-<div class="main-container p-5">
-      <div class="contant_1">
-        <div class="container mt-5">
-          <div class="row">
-            <h1 class="my-5">Let's begin creating powerful shorten URL !</h1>
-            <p>
-              Stay in control of your links with advanced features for
-              shortening, targeting, and tracking.
-            </p>
-            <div class="mb-3 mt-4 row">
-              <div class="col-sm-10">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="inputPassword"
-                />
-              </div>
-              <button type="button" class="btn btn-primary col-sm-2">
-                <a href="{{route('login')}}">Shorten</a>
-              </button>
+    <div class="main-container p-5">
+        <div class="contant_1">
+            <div class="container mt-5">
+                <div class="row">
+                    <h1 class="my-5">Let's begin creating powerful shorten URL !</h1>
+                    <p>
+                        Stay in control of your links with advanced features for
+                        shortening, targeting, and tracking.
+                    </p>
+                    <div class="mb-3 mt-4 row">
+                        <div class="col-sm-10">
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="inputPassword" />
+                        </div>
+                        <button type="button" class="btn btn-primary col-sm-2">
+                            <a href="{{route('login')}}">Shorten</a>
+                        </button>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <div class="contant_2">
-        <div class="container mt-5">
-          <div class="row">
-            <div class="col-lg-4 col-md-6 mb-3">
-              <div class="card py-4 px-lg-5 h-100">
-                <div class="card-body d-flex flex-column">
-                  <div class="text-center">
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1HswgEjS9kRoAKUDOMmDhnhUyyah7wBW9"
-                      class="img-fluid mb-5"
-                      alt="Websearch"
-                    />
-                  </div>
+        <div class="contant_2">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mb-3">
+                        <div class="card py-4 px-lg-5 h-100">
+                            <div class="card-body d-flex flex-column">
+                                <div class="text-center">
+                                    <img
+                                        src="https://drive.google.com/uc?export=view&id=1HswgEjS9kRoAKUDOMmDhnhUyyah7wBW9"
+                                        class="img-fluid mb-5"
+                                        alt="Websearch" />
+                                </div>
 
-                  <div class="card-title mb-4 text-center fs-2">
-                    Personal Package
-                  </div>
-                  <div class="pricing">
-                    <ul class="list-unstyled">
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">5 Links</span>
-                      </li>
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">5 Spaces</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="text-center mt-auto mb-4">
-                    <span class="fw-bold fs-2">$7</span>/month
-                  </div>
-                  <div class="text-center">
-                    <button type="button" class="btn btncolor">
-                      Choose Plan
-                    </button>
-                  </div>
+                                <div class="card-title mb-4 text-center fs-2">
+                                    Personal Package
+                                </div>
+                                <div class="pricing">
+                                    <ul class="list-unstyled">
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">5 Links</span>
+                                        </li>
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">5 Spaces</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="text-center mt-auto mb-4">
+                                    <span class="fw-bold fs-2">$7</span>/month
+                                </div>
+                                <div class="text-center">
+                                    <button type="button" class="btn btncolor">
+                                        Choose Plan
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 mb-3">
+                        <div class="card py-4 px-lg-5 h-100">
+                            <div class="card-body d-flex flex-column">
+                                <div class="text-center">
+                                    <img
+                                        src="https://drive.google.com/uc?export=view&id=1HswgEjS9kRoAKUDOMmDhnhUyyah7wBW9"
+                                        class="img-fluid mb-5"
+                                        alt="Websearch" />
+                                </div>
+
+                                <div class="card-title mb-4 text-center fs-2">
+                                    Partner Package
+                                </div>
+                                <div class="pricing">
+                                    <ul class="list-unstyled">
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">10 Links</span>
+                                        </li>
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">10 Spaces</span>
+                                        </li>
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">Link Stats</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="text-center mt-auto mb-4">
+                                    <span class="fw-bold fs-2">$11</span>/month
+                                </div>
+                                <div class="text-center">
+                                    <button type="button" class="btn btncolor">
+                                        Choose Plan
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 mb-3">
+                        <div class="card py-4 px-lg-5 h-100">
+                            <div class="card-body d-flex flex-column">
+                                <div class="text-center">
+                                    <img
+                                        src="https://drive.google.com/uc?export=view&id=1HswgEjS9kRoAKUDOMmDhnhUyyah7wBW9"
+                                        class="img-fluid mb-5"
+                                        alt="Websearch" />
+                                </div>
+
+                                <div class="card-title mb-4 text-center fs-2">
+                                    Team Package
+                                </div>
+                                <div class="pricing">
+                                    <ul class="list-unstyled">
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">50 Links</span>
+                                        </li>
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">25 Spaces</span>
+                                        </li>
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">Link Stats</span>
+                                        </li>
+                                        <li class="mb-3">
+                                            <i class="bx bxs-check-circle icon-color"></i>
+                                            <span class="small ms-3">Link Expiration</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="text-center mt-auto mb-4">
+                                    <span class="fw-bold fs-2">$15</span>/month
+                                </div>
+                                <div class="text-center">
+                                    <button type="button" class="btn btncolor">
+                                        Choose Plan
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-
-            <div class="col-lg-4 col-md-6 mb-3">
-              <div class="card py-4 px-lg-5 h-100">
-                <div class="card-body d-flex flex-column">
-                  <div class="text-center">
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1HswgEjS9kRoAKUDOMmDhnhUyyah7wBW9"
-                      class="img-fluid mb-5"
-                      alt="Websearch"
-                    />
-                  </div>
-
-                  <div class="card-title mb-4 text-center fs-2">
-                    Partner Package
-                  </div>
-                  <div class="pricing">
-                    <ul class="list-unstyled">
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">10 Links</span>
-                      </li>
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">10 Spaces</span>
-                      </li>
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">Link Stats</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="text-center mt-auto mb-4">
-                    <span class="fw-bold fs-2">$11</span>/month
-                  </div>
-                  <div class="text-center">
-                    <button type="button" class="btn btncolor">
-                      Choose Plan
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-3">
-              <div class="card py-4 px-lg-5 h-100">
-                <div class="card-body d-flex flex-column">
-                  <div class="text-center">
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1HswgEjS9kRoAKUDOMmDhnhUyyah7wBW9"
-                      class="img-fluid mb-5"
-                      alt="Websearch"
-                    />
-                  </div>
-
-                  <div class="card-title mb-4 text-center fs-2">
-                    Team Package
-                  </div>
-                  <div class="pricing">
-                    <ul class="list-unstyled">
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">50 Links</span>
-                      </li>
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">25 Spaces</span>
-                      </li>
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">Link Stats</span>
-                      </li>
-                      <li class="mb-3">
-                        <i class="bx bxs-check-circle icon-color"></i>
-                        <span class="small ms-3">Link Expiration</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="text-center mt-auto mb-4">
-                    <span class="fw-bold fs-2">$15</span>/month
-                  </div>
-                  <div class="text-center">
-                    <button type="button" class="btn btncolor">
-                      Choose Plan
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-</div>
+    </div>
 </x-backend>
