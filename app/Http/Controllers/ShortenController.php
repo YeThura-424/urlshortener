a14f14c2da166c3ce11ea6c77898637c3b663f30
YeThura-424 <rathanpoudle.26@gmail.com>
2023-09-29 12:12:00 +0630

    update:shorten controller

--- app/Http/Controllers/ShortenController.php
+++ app/Http/Controllers/ShortenController.php
@@ -22,44 +22,10 @@ class ShortenController extends Controller
      */
     public function store(Request $request)
     {
-        // dd($request->url);
         $url = $request->url;
         $builder = new \AshAllenDesign\ShortURL\Classes\Builder();
         $shortURLObject = $builder->destinationUrl($url)->make();
         $shortURL = $shortURLObject->default_short_url;
-        // dd($shortURL);
         return redirect()->route('home');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
